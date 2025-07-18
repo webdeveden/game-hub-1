046de4f3d4658625242e51a626e6703de654393d
Eden Abwe <edenjardin089@gmail.com>
2025-07-18 18:18:55 -0400

    Refactoring fetch game with ReactQuery

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,7 @@
+import { useQueries, useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 
 interface Platform {
   id: number;
@@ -6,6 +9,17 @@ interface Platform {
   slug: string;
 }
 
-const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+// const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+//using react-query to fetch platforms
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms")
+        .then((res) => res.data), // Simulating an API call with static data
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    initialData: { count: platforms.length, results: platforms }, // Initial data to avoid flickering
+  });
 
 export default usePlatforms;
